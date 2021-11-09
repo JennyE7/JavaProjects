0bd4a68117dbb109fa5b0f1dfdd0e1f3ce04b861
JennyE7 <jenniferjayne.me@hotmail.com>
2021-11-09 11:06:09 +0000

    Person Task - finished

--- Person.java
+++ Person.java
@@ -2,8 +2,34 @@
 public class Person {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	}
+	
+	private double height;
+	private String name;
+	private double shoeSize; 
+	private String jobTitle;
+	private int age;
+	
+	public Person (double height, String name, double shoeSize, String jobTitle, int age) {
+		this.height = height;
+		this.name = name;
+		this.shoeSize = shoeSize;
+		this.jobTitle = jobTitle;
+		this.age = age;
+	}
+	
+	public Person(String name, double height) {
+		this.name = name;
+		this.height = height;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
 	}
 
 }
--- Runner.java
+++ Runner.java
@@ -2,7 +2,11 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Person john = new Person("John", 160);
+		Person carl = new Person(155, "Carl", 8, "CEO", 45);
+		Person linda = new Person("Linda", 153);
+		Person amelia = new Person(162, "Amelia", 6, "Software Engineer", 28);
 
 	}
 
